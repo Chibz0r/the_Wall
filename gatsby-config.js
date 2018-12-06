2948875daea833346b0019b7476f0f649269a133
Nathan <nathan@atecdesign.co.uk>
2018-12-06 16:27:05 +0000

    Short name update

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'great-wall-of-yarmouth',
+        short_name: 'Great wall',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
